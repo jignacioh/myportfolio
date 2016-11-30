3b1ae9d07c7eb7a2876c1565da84c9652ec2ca63
USERSAD\jhurtace <jhurtace@everis.com>
2016-11-30 11:05:36 -0500

    Few change api client

---
+++ app/src/main/java/com/udacity/porfolioapp/service/ApiClient.java
@@ -0,0 +1,33 @@
+package com.udacity.porfolioapp.service;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by jhurtace on 30/11/2016.
+ */
+public class ApiClient {
+
+    public static final String BASE_URL = "https://waspiest-assignment.000webhostapp.com";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(getRequestHeader())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+    private static OkHttpClient getRequestHeader() {
+        OkHttpClient httpClient = new OkHttpClient();
+        // httpClient.setConnectTimeout(20, TimeUnit.SECONDS);
+        // httpClient.setReadTimeout(30, TimeUnit.SECONDS);
+
+        return httpClient;
+    }
+    
+}
