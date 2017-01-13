b7237499ca84dda46be52ac430bcf63efc5a8d32
USERSAD\jhurtace <jhurtace@everis.com>
2017-01-13 15:19:43 -0500

    boton +

--- app/src/main/java/com/udacity/porfolioapp/fragment/ListMoviesFragment.java
+++ app/src/main/java/com/udacity/porfolioapp/fragment/ListMoviesFragment.java
@@ -9,12 +9,10 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.udacity.porfolioapp.ListMoviesActivity;
 import com.udacity.porfolioapp.OnMovieClickListener;
 import com.udacity.porfolioapp.R;
 import com.udacity.porfolioapp.model.ListMovie;
@@ -24,7 +22,6 @@ import com.udacity.porfolioapp.ui.adapter.MovieRecyclerViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -85,27 +82,27 @@ public class ListMoviesFragment extends Fragment implements Callback<ListMovie>,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_listmovies_list, container, false);
 
-            Context context = view.getContext();
-            rvMovies= (RecyclerView) view.findViewById(R.id.rvMovies);
-            if (mColumnCount <= 1) {
-                rvMovies.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                rvMovies.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
+        Context context = view.getContext();
+        rvMovies= (RecyclerView) view.findViewById(R.id.rvMovies);
+        if (mColumnCount <= 1) {
+            rvMovies.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            rvMovies.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+        }
 
-            retrofit = new Retrofit.Builder()
+        retrofit = new Retrofit.Builder()
                     .baseUrl("https://waspiest-assignment.000webhostapp.com")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
 
-            listMovies=new ArrayList<>();
+        listMovies=new ArrayList<>();
 
-            //lLayout = new GridLayoutManager(context, 2);
-            swipeContainer=(SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
-            rvMovies.setHasFixedSize(true);
-           // rvMovies.setLayoutManager(lLayout);
+        //lLayout = new GridLayoutManager(context, 2);
+        swipeContainer=(SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        rvMovies.setHasFixedSize(true);
+        // rvMovies.setLayoutManager(lLayout);
 
-            swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
                 @Override
                 public void onRefresh() {
                     rvMovies.setVisibility(View.GONE);
@@ -114,11 +111,76 @@ public class ListMoviesFragment extends Fragment implements Callback<ListMovie>,
                     call.enqueue(ListMoviesFragment.this);
                 }
             });
-            swipeContainer.setRefreshing(true);
-            MovieRestAPI stackOverflowAPI = retrofit.create(MovieRestAPI.class);
-            Call<ListMovie> call = stackOverflowAPI.loadMovies();
-            call.enqueue(ListMoviesFragment.this);
+        swipeContainer.setRefreshing(true);
+        MovieRestAPI stackOverflowAPI = retrofit.create(MovieRestAPI.class);
+        Call<ListMovie> call = stackOverflowAPI.loadMovies();
+        call.enqueue(ListMoviesFragment.this);
 
+        /*
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.setter);
+        button.setSize(FloatingActionButton.SIZE_MINI);
+        button.setColorNormalResId(R.color.pink);
+        button.setColorPressedResId(R.color.pink_pressed);
+        button.setIcon(R.drawable.ic_fab_star);
+        button.setStrokeVisible(false);
+
+        final View actionB = findViewById(R.id.action_b);
+
+        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
+        actionC.setTitle("Hide/Show Action above");
+        actionC.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu)view.findViewById(R.id.multiple_actions);
+        menuMultipleActions.addButton(actionC);
+
+        final FloatingActionButton removeAction = (FloatingActionButton) findViewById(R.id.button_remove);
+        removeAction.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((FloatingActionsMenu) findViewById(R.id.multiple_actions_down)).removeButton(removeAction);
+            }
+        });
+
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        drawable.getPaint().setColor(getResources().getColor(R.color.white));
+        ((FloatingActionButton) findViewById(R.id.setter_drawable)).setIconDrawable(drawable);
+
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionA.setTitle("Action A clicked");
+            }
+        });
+
+        // Test that FAMs containing FABs with visibility GONE do not cause crashes
+        findViewById(R.id.button_gone).setVisibility(View.GONE);
+
+        final FloatingActionButton actionEnable = (FloatingActionButton) findViewById(R.id.action_enable);
+        actionEnable.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuMultipleActions.setEnabled(!menuMultipleActions.isEnabled());
+            }
+        });
+
+        FloatingActionsMenu rightLabels = (FloatingActionsMenu) findViewById(R.id.right_labels);
+        FloatingActionButton addedOnce = new FloatingActionButton(this);
+        addedOnce.setTitle("Added once");
+        rightLabels.addButton(addedOnce);
+
+        FloatingActionButton addedTwice = new FloatingActionButton(this);
+        addedTwice.setTitle("Added twice");
+        rightLabels.addButton(addedTwice);
+        rightLabels.removeButton(addedTwice);
+        rightLabels.addButton(addedTwice);
+
+        */
         return view;
     }
     @Override
